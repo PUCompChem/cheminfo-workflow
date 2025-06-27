93bd3a26cfae9d8b3b85adf48efd0b58d761c94c
Lyudvika <lydvika@abv.bg>
2025-06-27 15:35:34 +0300

    Add new task convert_xlsx_to_csv.py

---
+++ cheminfo-analysis/tasks/convert_xlsx_to_csv.py
@@ -0,0 +1,17 @@
+# + tags=["parameters"]
+upstream = None
+product = None
+folder_input = None
+
+# -
+
+import pandas as pd
+import os
+import glob
+
+os.makedirs(product['converted_xlsx_to_csv'], exist_ok=True)
+
+for file_path in glob.glob(os.path.join(folder_input, '*.xlsx')):
+    df = pd.read_excel(file_path, engine='openpyxl')
+    output_filename = os.path.splitext(os.path.basename(file_path))[0] + ".csv"
+    df.to_csv(os.path.join(product['converted_xlsx_to_csv'], output_filename), index=False)
