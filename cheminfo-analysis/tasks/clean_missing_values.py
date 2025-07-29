d7e7f198b5d43e3a800c18bf62f828645bf2e08c
Lyudvika <lyudiacc@gmail.com>
2025-07-29 13:50:23 +0300

    Update tasks clean_missing_values.py

--- cheminfo-analysis/tasks/clean_missing_values.py
+++ cheminfo-analysis/tasks/clean_missing_values.py
@@ -1,7 +1,9 @@
 # + tags=["parameters"]
 upstream = ['process_data']
 product = None
-
+target_column = None
+id_column = ''
+list_column = None
 # -
 
 import os
@@ -13,7 +15,16 @@ os.makedirs(product['cleaned_csv'], exist_ok=True)
 for file_path in glob.glob(os.path.join(upstream['process_data']['processed_data'], '*.csv')):
     df = pd.read_csv(file_path)
 
-    df_clean = df.dropna()
+    target_cols = set(list_column or [])
+    if target_column:
+        target_cols.add(target_column)
+    if id_column:
+        target_cols.add(id_column)
+
+    if target_cols:
+        df_clean = df.dropna(subset=target_cols)
+    else:
+        df_clean = df.copy()
 
     output_filename = os.path.basename(file_path)
     df_clean.to_csv(os.path.join(product['cleaned_csv'], output_filename), index=False)
